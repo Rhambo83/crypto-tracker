c693f02d58e5f6496eba9a9245ff048d95e58552
Rhambo83 <dharmse@gmail.com>
2025-04-20 01:43:56 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Crypto Tracker App</h1>
+      <p>Coming soon: Real-time market heatmaps!</p>
+    </div>
+  );
+}
+
+export default App;
